3502393ae2bf1acb0228fb6a9c2d6041b7c6aa28
Chris Frederick <cfreder2@cfreder2s-mbp.dhcp.nd.edu>
2014-04-22 18:52:19 -0400

    Added twitter_search.py

--- twitter_auth.py
+++ twitter_auth.py
@@ -1,7 +1,7 @@
 
 # You need to add your own api keys here.  You can get them at dev.twitter.com
 
-OAUTH_KEYS = {'api_key': 'Insert your key between the quotes',
-              'api_secret': 'Insert your key between the quotes',
-              'access_token_key': 'Insert your key between the quotes',
-              'access_token_secret': 'Insert your key between the quotes'}
+OAUTH_KEYS = {'api_key': 'Insert api key here',
+              'api_secret': 'Insert api secret here',
+              'access_token_key': 'Insert access token key',
+              'access_token_secret': 'access token secret here'}
