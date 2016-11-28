6e907d30574b92955cbd5a6d74faabc9daaabd16
Chris Frederick <cfreder2@cfreder2-mbpro-2014.dhcp.nd.edu>
2016-11-28 11:19:44 -0500

    fixed secret key

--- twitter_auth.py
+++ twitter_auth.py
@@ -1,7 +1,7 @@
 
 # You need to add your own api keys here.  You can get them at dev.twitter.com
 
-OAUTH_KEYS = {'api_key': 'Qa6CVgg5i06LptWXAkj7A',
-              'api_secret': 'MQ7MAxhmpkIpNoqFMFlZZ3Yo5Wnpcs63zDOh5IhLYNw',
-              'access_token_key': '1967945953-vtB5jUzRBsVmRcivUmGTAVUgu0vx91G3KXKo3xY',
-              'access_token_secret': 'KmJumdgH59Nzokdolq1uitOYmNl0BNlSTdxbeTAlPuKm0'}
+OAUTH_KEYS = {'api_key': 'copy and past api key here',
+              'api_secret': 'copy and paste api secret here',
+              'access_token_key': 'copy and paste access token key here',
+              'access_token_secret': 'copy and paste access token secret here'}
